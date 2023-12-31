cac62d37c16e68b98160cc71672a89289ada53ac
coder-saim <saim.cse.du.744@gmail.com>
2023-12-31 11:50:55 +0600

    products endpoint implemented

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
+const products = require('./routes/products');
+
+app.use(express.json())
+app.use('/api/v1',products);
+
+
+
+
+
+
+
 
 module.exports = app
+
+
+
+
