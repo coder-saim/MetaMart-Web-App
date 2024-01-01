f79cc83ef8e60f605dddd2f5881094a79e09f77a
coder-saim <saim.cse.du.744@gmail.com>
2024-01-01 12:20:31 +0600

    Handle Uncaught exceptions

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
 dotenv.config({ path: "config/config.env" });
 
 connectDatabase();
@@ -12,11 +19,12 @@ const server = app.listen(process.env.PORT, () => {
   );
 });
 
+
 // Handle Unhandled Promise rejections
 process.on("unhandledRejection", (err) => {
   console.log(`ERROR: ${err.stack}`);
   console.log("Shutting down the server due to Unhandled Promise rejection");
   server.close(() => {
     process.exit(1);
-  });  
+  });
 });
