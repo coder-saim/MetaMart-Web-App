cf8754dfcd6981862eaffbabacff62436ce06beb
coder-saim <saim.cse.du.744@gmail.com>
2024-01-01 12:17:43 +0600

    Handle Unhandled Promise rejections

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-const connectDatabase = require('./config/database');
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
-dotenv.config({path: 'config/config.env'})
+dotenv.config({ path: "config/config.env" });
 
-connectDatabase();  
+connectDatabase();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
-})  
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
 
+// Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });  
+});
