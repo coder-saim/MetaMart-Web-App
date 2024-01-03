ddab2d9edcf1eb494faa00737618e884ba442735
coder-saim <saim.cse.du.744@gmail.com>
2024-01-03 12:33:38 +0600

    jwt token added to the cookie for later use

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middlewares/catchAsyncError");
-
+const sendToken = require('../utils/jwtToken');
 
 exports.registerUser = catchAsyncError(async(req,res,next) => {
     const {name,email,password} = req.body;
@@ -18,20 +18,13 @@ exports.registerUser = catchAsyncError(async(req,res,next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        user,
-        token
-    })
+    sendToken(user,201,res);
 })
 
 
 
 exports.loginUser = catchAsyncError(async(req,res,next) => {
     const {email,password} = req.body;
-    let random_id = Math.floor((Math.random() * 10000) + 1);
     
     if(!email && !password){
         return next(new ErrorHandler('Please Enter email and password!',400));
@@ -49,10 +42,5 @@ exports.loginUser = catchAsyncError(async(req,res,next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token = user.getJwtToken();
-
-    res.status(200).json({
-        success: true,
-        token
-    })
+    sendToken(user,200,res);
 })
