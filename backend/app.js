3d3201f6f82b3fe6b0e950840b1dd25ddf0afa12
coder-saim <saim.cse.du.744@gmail.com>
2024-01-03 11:50:11 +0600

    auth user implemented and password is encrypted..

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const products = require('./routes/products');
+const auth = require('./routes/auth');
 const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json())
 app.use('/api/v1',products);
+app.use('/api/v1',auth);
+
+
+
 
 // middleeare to handle errros
 app.use(errorMiddleware);
-
-
 
 
 
---
+++ backend/controllers/authController.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("../middlewares/catchAsyncError");
+
+
+exports.registerUser = catchAsyncError(async(req,res,next) => {
+    const {name,email,password} = req.body;
+    let random_id = Math.floor((Math.random() * 10000) + 1);
+    
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: random_id,
+            url: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww'
+
+        }
+    })
+
+    res.status(201).json({
+        success: true,
+        user
+    })
+})
