da36603052868a4684e7117afa1b3260c97a112f
coder-saim <saim.cse.du.744@gmail.com>
2024-01-04 11:53:39 +0600

    admins get, put and delete user functionality added...

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 const {
   registerUser,
   loginUser,
@@ -9,7 +9,11 @@ const {
   resetPassword,
   getUserProfile,
   updatePassword,
-  updateProfile
+  updateProfile,
+  allUsers,
+  getUserDetails,
+  updateUserProfile,
+  deleteUser
 } = require("../controllers/authController");
 
 router.route("/register").post(registerUser);
@@ -18,9 +22,19 @@ router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
-
 router.route("/profile").get(isAuthenticatedUser, getUserProfile);
 router.route("/profile/update").put(isAuthenticatedUser, updateProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserProfile)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser)
+
+
 
 module.exports = router;
