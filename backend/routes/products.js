84abfb4411ff6afb53a1d0b65f2f8418577233ec
coder-saim <saim.cse.du.744@gmail.com>
2024-01-06 12:47:21 +0600

    review created successfully

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { newProduct,getAllProducts,getSingleProduct,updateProduct,deleteProduct } = require('../controllers/productController')
-const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
+const {
+  newProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
-router.route('/allProducts').get(getAllProducts);
-router.route('/product/:id').get(getSingleProduct);
-router.route('/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),newProduct);
-router.route('/product/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct);
-router.route('/product/:id').delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct);
+router.route("/allProducts").get(getAllProducts);
+router.route("/product/:id").get(getSingleProduct);
+router
+.route("/product/new")
+.post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+router
+.route("/product/:id")
+.put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+router
+.route("/product/:id")
+.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+
+router.route("/review").put(isAuthenticatedUser,createProductReview);
 
 module.exports = router;
-
-
- 
-
-
-
-
-
-
-
-
-
-
