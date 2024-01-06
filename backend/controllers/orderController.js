41d1732c1699863628f60852b346a67be2181566
coder-saim <saim.cse.du.744@gmail.com>
2024-01-06 11:42:28 +0600

    get all orders by user and admin implemented....

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,7 +37,46 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
 
 
 
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
 
+    if (!order) {
+        return next(new ErrorHandler('No Order found with this ID', 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user.id })
+
+    res.status(200).json({
+        success: true,
+        orders
+    })
+})
+
+
+
+
+exports.allOrders = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find()
+
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders
+    })
+})
 
 
 
