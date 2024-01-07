dd6fa1c0a0ee92df2a27eb3e96d54aa5494fdca6
coder-saim <saim.cse.du.744@gmail.com>
2024-01-07 14:36:14 +0600

    header component created..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Header from './components/layout/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>MetaMart</h1>
+      <Header/>
     </div>
   );
 } 
