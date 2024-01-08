1d106fbd9958c363907d3ee5f3ffdd7d708c429d
coder-saim <saim.cse.du.744@gmail.com>
2024-01-08 13:05:54 +0600

    product reducer added

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -3,12 +3,12 @@ const ErrorHandler = require('../utils/errorHandler');
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
 
-    res.status(err.statusCode).json({
-        success: false,
-        error: err,
-        errMessage: err.message,
-        stack: err.stack
-    })
+    // res.status(err.statusCode).json({
+    //     success: false,
+    //     error: err,
+    //     errMessage: err.message,
+    //     stack: err.stack
+    // })
     
     // if (process.env.NODE_ENV === 'DEVELOPMENT') {
     //     res.status(err.statusCode).json({
@@ -19,7 +19,7 @@ module.exports = (err, req, res, next) => {
     //     })
     // } 
 
-    if (process.env.NODE_ENV === 'PRODUCTION') {
+    if (true) {
         let error = { ...err }
 
         error.message = err.message;
---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import {
+    ALL_PRODUCTS_REQUEST,
+    ALL_PRODUCTS_SUCCESS,
+    ALL_PRODUCTS_FAIL,
+    ADMIN_PRODUCTS_REQUEST,
+    ADMIN_PRODUCTS_SUCCESS,
+    ADMIN_PRODUCTS_FAIL,
+    NEW_PRODUCT_REQUEST,
+    NEW_PRODUCT_SUCCESS,
+    NEW_PRODUCT_RESET,
+    NEW_PRODUCT_FAIL,
+    DELETE_PRODUCT_REQUEST,
+    DELETE_PRODUCT_SUCCESS,
+    DELETE_PRODUCT_RESET,
+    DELETE_PRODUCT_FAIL,
+    UPDATE_PRODUCT_REQUEST,
+    UPDATE_PRODUCT_SUCCESS,
+    UPDATE_PRODUCT_RESET,
+    UPDATE_PRODUCT_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    NEW_REVIEW_REQUEST,
+    NEW_REVIEW_SUCCESS,
+    NEW_REVIEW_RESET,
+    NEW_REVIEW_FAIL,
+    GET_REVIEWS_REQUEST,
+    GET_REVIEWS_SUCCESS,
+    GET_REVIEWS_FAIL,
+    DELETE_REVIEW_REQUEST,
+    DELETE_REVIEW_SUCCESS,
+    DELETE_REVIEW_RESET,
+    DELETE_REVIEW_FAIL,
+    CLEAR_ERRORS
+
+} from '../constants/productConstants'
+
+export const getProducts = () => async (dispatch) => {
+    try {
+
+        dispatch({ type: ALL_PRODUCTS_REQUEST })
+        const { data } = await axios.get('api/v1/allProducts')
+
+        dispatch({
+            type: ALL_PRODUCTS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ALL_PRODUCTS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+
+// Clear Errors
+export const clearErrors = () => async (dispatch) => {
+    dispatch({
+        type: CLEAR_ERRORS
+    })
+}
