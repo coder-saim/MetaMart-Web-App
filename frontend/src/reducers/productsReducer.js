0054141efeee65724fc0d4dbe99a6bb01a76dcf9
coder-saim <saim.cse.du.744@gmail.com>
2024-01-22 18:55:26 +0600

    modified

--- frontend/src/reducers/productsReducer.js
+++ frontend/src/reducers/productsReducer.js
@@ -80,6 +80,62 @@ export const productsReducer = (state = { products: [] }, action) => {
 }
 
 
+export const productReducer = (state = {}, action) => {
+    switch (action.type) {
+
+        case DELETE_PRODUCT_REQUEST:
+        case UPDATE_PRODUCT_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case DELETE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isDeleted: action.payload
+            }
+
+        case UPDATE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isUpdated: action.payload
+            }
+
+
+        case DELETE_PRODUCT_FAIL:
+        case UPDATE_PRODUCT_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+
+        case DELETE_PRODUCT_RESET:
+            return {
+                ...state,
+                isDeleted: false
+            }
+
+        case UPDATE_PRODUCT_RESET:
+            return {
+                ...state,
+                isUpdated: false
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state
+    }
+}
+
+
 export const productDetailsReducer = (state = { product: {} }, action) => {
     switch (action.type) {
 
