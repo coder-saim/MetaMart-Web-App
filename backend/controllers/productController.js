197a37b1fa93c413ceea49052a822958bcd9b326
coder-saim <saim.cse.du.744@gmail.com>
2024-01-22 22:10:55 +0600

    admin get all product implemented

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ const APIFeatures = require("../utils/apiFeatures");
 
 exports.newProduct = catchAsyncError(async (req, res, next) => {
   req.body.user = req.user.id;
-
+  
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
