3f712f0793f52651d369b8f1ce1a5a7b9e6a8a4d
coder-saim <saim.cse.du.744@gmail.com>
2024-01-24 23:14:25 +0600

    user update implemented

---
+++ frontend/src/components/admin/UpdateUser.jsx
@@ -0,0 +1,152 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import Metadata from "../layout/Metadata";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateUser,
+  getUserDetails,
+  clearErrors,
+} from "../../actions/userActions";
+import { UPDATE_USER_RESET } from "../../constants/userConstants";
+import { Slide, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate, useParams } from "react-router-dom";
+
+const UpdateUser = ({ history, match }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const { error, isUpdated } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.userDetails);
+
+  const userId = id;
+
+  useEffect(() => {
+    console.log(user && user._id !== userId);
+    if (user && user._id !== userId) {
+      dispatch(getUserDetails(userId));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setRole(user.role);
+    }
+
+    if (error) {
+      toast(error, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      dispatch(clearErrors());
+    }
+
+    if (isUpdated) {
+      toast("User updated successfully", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      navigate("/admin/users");
+
+      dispatch({
+        type: UPDATE_USER_RESET,
+      });
+    }
+  }, [dispatch, error, isUpdated, userId, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.set("name", name);
+    formData.set("email", email);
+    formData.set("role", role);
+
+    dispatch(updateUser(user._id, formData));
+  };
+
+  return (
+    <Fragment>
+      <Metadata title={`Update User`} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+
+        <div className="col-12 col-md-10">
+          <div className="row wrapper">
+            <div className="col-10 col-lg-5">
+              <form className="shadow-lg" onSubmit={submitHandler}>
+                <h1 className="mt-2 mb-5">Update User</h1>
+
+                <div className="form-group">
+                  <label htmlFor="name_field">Name</label>
+                  <input
+                    type="name"
+                    id="name_field"
+                    className="form-control"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email_field">Email</label>
+                  <input
+                    type="email"
+                    id="email_field"
+                    className="form-control"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="role_field">Role</label>
+
+                  <select
+                    id="role_field"
+                    className="form-control"
+                    name="role"
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                  >
+                    <option value="user">user</option>
+                    <option value="admin">admin</option>
+                  </select>
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn update-btn btn-block mt-4 mb-3"
+                >
+                  Update
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </Fragment>
+  );
+};
+
+export default UpdateUser;
